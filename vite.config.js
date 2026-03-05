4fb4f591f799016be2cd8ae733a75d586ce76433
ristinalapbulan-create <ristinalapbulan@gmail.com>
2026-03-05 22:58:18 +0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Tambahkan ini agar aman saat di-deploy ke sub-folder GitHub Pages
+  base: './',
 })

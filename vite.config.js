09c52b96016114996a74548979cf73820249bbe9
ristinalapbulan-create <ristinalapbulan@gmail.com>
2026-03-05 22:54:36 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Pastikan TIDAK ADA tulisan "base: ..." di sini
+  base: './', // Tambahkan ini agar aman saat di-deploy ke sub-folder GitHub Pages
 })
